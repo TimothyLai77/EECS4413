665b46e32bb55e53032b013b7d3eb76be3e876a7
Tim <lai.timothy77@gmail.com>
2024-07-07 17:51:01 -0400

    switching computer commit

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
 const inventoryController = require("./server/controller/inventoryManagement");
-
+const { Sequelize } = require('sequelize');
 
 const PORT = 8080;
 const CLIENT_FRONTEND_PATH = path.join(__dirname, "client", "build");
+const sequelize = new Sequelize('db', 'root', 'password', {
+    host: 'db',
+    dialect: 'mariadb'
+});
+
+async function connectToDb(){
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
 
 function prepareApp(){
     const app = express();
@@ -16,6 +28,8 @@ function prepareApp(){
 
     app.use("/", express.static(CLIENT_FRONTEND_PATH))
     inventoryController(app);
+    connectToDb();
+
     return app;
 
 }
