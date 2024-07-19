0833bd2820ae333e293a66bc3d419642139335ad
Tim <lai.timothy77@gmail.com>
2024-07-18 22:28:57 -0400

    comments for addUser delete user function

--- server/modules/UserManager.js
+++ server/modules/UserManager.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const { User } = require('../data/sequelizeModels/User');
 const uniqid = require('uniqid');
-const { EmailAlreadyExistsError } = require('../modules/errors')
+const { EmailAlreadyExistsError, UserDoesNotExistError } = require('../modules/errors')
 
 const createNewUser = async (firstName, lastName, email, password) => {
 
@@ -12,8 +12,9 @@ const createNewUser = async (firstName, lastName, email, password) => {
             email: email
         }
     });
-    // email does not exist in db, 
+
     if (!duplicateUserCheck) {
+        // if email does not exist in the DB 
         const userId = uniqid('user-');
         newUser = await User.create({
             userId: userId,
@@ -27,8 +28,17 @@ const createNewUser = async (firstName, lastName, email, password) => {
     }
 };
 
-// const deleteUser = async (User) => {
-//     const userToDelete()
-// };
+const deleteUser = async (User) => {
+    const userToDelete = await User.findByPk(User.userId);
+    if (!userToDelete) {
+        // if user does exist, delete and save changes
+        await userToDelete.destroy();
+        await userToDelete.save();
+    } else {
+        // user does not exist
+        throw new UserDoesNotExistError;
+    }
+};
 
 exports.createNewUser = createNewUser;
+exports.deleteUser = deleteUser;
--- server/modules/errors.js
+++ server/modules/errors.js
@@ -6,5 +6,10 @@ class OutOfStockError extends Error {
 class EmailAlreadyExistsError extends Error {
 
 }
+
+class UserDoesNotExistError extends Error {
+
+}
 exports.OutOfStockError = OutOfStockError;
 exports.EmailAlreadyExistsError = EmailAlreadyExistsError;
+exports.UserDoesNotExistError = UserDoesNotExistError;
