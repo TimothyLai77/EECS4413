e06edc57e19b72555b32f0a9c33f080b64f4fb20
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2024-07-28 13:52:34 -0400

    added admin inventory add and remove quantity redux logic

--- client/src/features/catalog.js
+++ client/src/features/catalog.js
@@ -18,33 +18,14 @@ const catalogSlice = createSlice({
                 // this return value is stored under action.payload
                 //  FORMAT for page render: 
                 // id: 4, name: 'Product 4', price: 14.99, brand:'BRAND',info:"This is the product information", image: image },
-
-
-                // FORMAT RETURNED FROM API:
-                // const x = {
-                //     "itemId": "item-14allkn0jlz4r6qzb",
-                //     "quantity": 7,
-                //     "createdAt": "2024-07-27T23:19:17.000Z",
-                //     "updatedAt": "2024-07-27T23:19:17.000Z",
-                //     "Item": {
-                //         "itemId": "item-14allkn0jlz4r6qzb",
-                //         "name": "hlkjh",
-                //         "price": 7777,
-                //         "brand": "h",
-                //         "description": "gkjhgkjhgk jhgk jhg",
-                //         "image": "jhgkjh",
-                //         "createdAt": "2024-07-27T23:19:17.000Z",
-                //         "updatedAt": "2024-07-27T23:19:17.000Z"
-                //     }
-                // },
-
                 data = data.inventory.map(item => ({
                     id: item.itemId,
                     name: item.Item.name,
                     price: item.Item.price,
                     brand: item.Item.brand,
                     info: item.Item.description,
-                    image: item.Item.image
+                    image: item.Item.image,
+                    stock: item.quantity,
                 }));
                 return data;
             },
@@ -58,14 +39,61 @@ const catalogSlice = createSlice({
                     console.log("Inventory failed to fetch");
                 }
             }
+        ),
+        inventoryAddStock: create.asyncThunk(async (item) => {
+            //item =  {itemID: string, amount: whole number}
+            //TODO: create the API for this, and make sure the backend parsing matches what is being sent
+            await axios.post("/api/inventory/add", { itemID: item.itemID, stock: item.amount });
+        }, {
+            // runs after asyncThunk is fulfilled, modify the state as needed
+            fulfilled: (state, action) => {
+                //redux only works properly if it actually detects NEW objects, best to create a new array, and do a shallow copy over
+                const newState = [...state.products];
+                //find the item that had stock added to
 
+                const newStockItem = newState.find(e => e.id === item.itemID);
+                if (newStockItem) {
+                    //if found (which it should) update the value on the front-end to prevent going to backend to refetch
+                    newStockItem.stock += item.amount;
+                    //set the new state and let redux trigger any re-renders
+                    state.products = newState;
+                }
 
+            },
+            // err handling if async thunk fails
+            rejected: () => {
+                console.log("inventory add failed");
+            }
+        }),
 
-        )
+        inventoryDeductStock: create.asyncThunk(async (item) => {
+            //item =  {itemID: string, amount: whole number}
+            //TODO: create the API for this, and make sure the backend parsing matches what is being sent
+            await axios.post("/api/inventory/deduct", { itemID: item.itemID, stock: item.amount });
+        }, {
+            // runs after asyncThunk is fulfilled, modify the state as needed
+            fulfilled: (state, action) => {
+                //redux only works properly if it actually detects NEW objects, best to create a new array, and do a shallow copy over
+                const newState = [...state.products];
+                //find the item that had stock added to
+                const newStockItem = newState.find(e => e.id === item.itemID);
+                if (newStockItem) {
+                    //if found (which it should) update the value on the front-end to prevent going to backend to refetch
+                    newStockItem.stock -= item.amount;
+                    //set the new state and let redux trigger any re-renders
+                    state.products = newState;
+                }
+
+            },
+            // err handling if async thunk fails
+            rejected: () => {
+                console.log("inventory duduction failed");
+            }
+        })
     })
 });
 
-export const { fetchInventory } = catalogSlice.actions;
+export const { fetchInventory, inventoryAddStock, inventoryDeductStock } = catalogSlice.actions;
 export default catalogSlice.reducer;
 
 
