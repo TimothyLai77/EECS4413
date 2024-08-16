88453e7aaeffc773c7f015cdb8b720787e1eb1b7
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2024-08-15 21:20:53 -0400

    transaction sorts by date

--- server/modules/TransactionManager.js
+++ server/modules/TransactionManager.js
@@ -1,13 +1,14 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const { User } = require('../data/sequelizeModels/User');
-const {Transaction } = require('../data/sequelizeModels/Transaction');
-const {LedgerEntry} = require('../data/sequelizeModels/LedgerEntry')
-const {Item} = require('../data/sequelizeModels/Item')
+const { Transaction } = require('../data/sequelizeModels/Transaction');
+const { LedgerEntry } = require('../data/sequelizeModels/LedgerEntry')
+const { Item } = require('../data/sequelizeModels/Item');
+const dayjs = require('dayjs');
 
 // customer side get all of their transactions
 const getUserTransactions = async (userId) => {
     const userModel = await User.findByPk(userId);
-    if(!userModel) throw new Error("user does not eixst");
+    if (!userModel) throw new Error("user does not eixst");
 
     const userTransactionList = await Transaction.findAll({
         where: {
@@ -31,12 +32,12 @@ const getUserTransactions = async (userId) => {
 const getTransactionDetails = async (transactionId) => {
     // check valid transaction
     const transaction = await Transaction.findByPk(transactionId);
-    if(!transaction) throw new Error("invalid transaction ID");
+    if (!transaction) throw new Error("invalid transaction ID");
 
     // get the details of the transaction requested
     const ledgerList = await LedgerEntry.findAll({
-        where:{
-            transactionId :transactionId
+        where: {
+            transactionId: transactionId
         }
     });
     //console.log(ledgerList);
@@ -57,12 +58,12 @@ const getTransactionDetails = async (transactionId) => {
 const getAllTransactions = async () => {
     const allTransactionModels = await Transaction.findAll({
         include: [
-            {model: LedgerEntry, include: Item},
-            {model: User}
+            { model: LedgerEntry, include: Item },
+            { model: User }
         ]
     });
     console.log(allTransactionModels);
-    const returnList = allTransactionModels.map((t) => {
+    let returnList = allTransactionModels.map((t) => {
         return {
             transactionId: t.transactionId,
             total: t.total,
@@ -78,6 +79,11 @@ const getAllTransactions = async () => {
                 quantity: itemBought.quantity
             }))
         }
+    });
+    returnList = returnList.sort((transactionA, transactionB) => {
+        const dateA = dayjs(transactionA.date).unix();
+        const dateB = dayjs(transactionB.date).unix();
+        return dateB - dateA;
     })
     return returnList;
 }
